0ae11bdb72cc61c95e88dcf1a4f89cf495b16107
arif.liminto <aliminto@gerejaoikos.org.au>
2017-11-29 21:09:24 +0800

    split javascript to different folder

---
+++ UsherSheat/UsherSheat/Script/seat.js
@@ -0,0 +1,92 @@
+﻿function seat()
+{
+    /** global variable **/
+    var totalPeople = 0;
+    var newPeople = 0;
+    var foreignPeople = 0;
+    var defaultRowNumber = 5;
+    var extraRowCol1 = 0;
+    var extraRowCol2 = 0;
+    var extraRowCol3 = 0;
+    var extraRowCol4 = 0;
+    var extraRowCol5 = 0;
+
+    this.seat = function (divid)
+    {
+        //change the element color of button divid and the value
+        //state : empty->seated->newpeople->empty
+        if ($(divid).text() == "O") {
+            $(divid).text('X');
+            $(divid).removeClass("label-success");
+            $(divid).addClass("label-danger");
+
+            totalPeople += 1;
+            $(txtTotalPeople).val(totalPeople);
+        }
+        else if ($(divid).text() == "X") {
+            $(divid).text('N');
+            $(divid).removeClass("label-danger");
+            $(divid).addClass("label-info");
+
+            newPeople += 1;
+            $(txtNewPeople).val(newPeople);
+        }
+        else if ($(divid).text() == "N") {
+            $(divid).text('T');
+            $(divid).removeClass("label-info");
+            $(divid).addClass("label-warning");
+
+            newPeople -= 1;
+            $(txtNewPeople).val(newPeople);
+
+            foreignPeople += 1;
+            $(txtForeignPeople).val(foreignPeople);
+
+        }
+        else {
+            $(divid).text('O');
+            $(divid).removeClass("label-warning");
+            $(divid).addClass("label-success");
+
+            totalPeople -= 1;
+            $(txtTotalPeople).val(totalPeople);
+
+            foreignPeople -= 1;
+            $(txtForeignPeople).val(foreignPeople);
+        }
+    }
+
+    //add seat dynamically depend on which table
+    this.addSeat = function (tableid, bigcolumn)
+    {
+        var row = defaultRowNumber;
+        if (bigcolumn == 1) {
+            extraRowCol1 += 1;
+            row += extraRowCol1;
+        }
+        else if (bigcolumn == 2) {
+            extraRowCol2 += 1;
+            row += extraRowCol2;
+        }
+
+
+        var x = buildSeat(bigcolumn, row);
+        var str = " <table class='table table-stripped'><tr><td>" + x + "</td></tr></table>";
+
+        $("#" + tableid).hide().append(str).fadeIn('slow');
+    }
+
+    this.buildSeat = function (bigcol, row)
+    {
+        var idstr = "tablecol" + bigcol + "row" + row + "col";
+
+        var str = " <label id='" + idstr + "1' class='label label-success' onclick='seat(this)'>O</label> " +
+            " <label id='" + idstr + "2' class='label label-success' onclick='seat(this)'>O</label>" +
+            " <label id='" + idstr + "3' class='label label-success' onclick='seat(this)'>O</label>" +
+            " <label id='" + idstr + "4' class='label label-success' onclick='seat(this)'>O</label>" +
+            " <label id='" + idstr + "5' class='label label-success' onclick='seat(this)'>O</label>";
+
+        //alert (str);
+        return str;
+    }
+}
