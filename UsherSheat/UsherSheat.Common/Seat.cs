59d27ffe37fea957298ecdea65898717bfa04a41
arif.liminto <aliminto@gerejaoikos.org.au>
2017-12-01 23:22:34 +0800

    Add Api project and common project

---
+++ UsherSheat/UsherSheat.Common/Seat.cs
@@ -0,0 +1,15 @@
+﻿namespace UsherSheat.Common
+{
+    public enum SeatStatus
+    {
+        Free = 0,
+        Occupied = 1,
+        NewPeople =2
+    }
+
+    public class Seat
+    {
+        public int SeatId { get; set; }
+        public SeatStatus Status { get; set; }
+    }
+}
